ebeca0310b62ff056c6318d03d49f91b6f65761a
Selin Kaya <selinkaya99@gmail.com>
2021-05-08 11:29:05 +0300

    Created the Visual Studio version of the project

---
+++ Bowling_VisualStudio/bowling/bowling/vshader.glsl
@@ -0,0 +1,14 @@
+#version 410
+
+in vec4 vPosition;
+out vec4 color;
+
+uniform mat4 ModelView;
+uniform mat4 Projection;
+uniform vec4 Color;
+
+void main()
+{
+    gl_Position = Projection * ModelView * vPosition;
+    color = Color;
+}
