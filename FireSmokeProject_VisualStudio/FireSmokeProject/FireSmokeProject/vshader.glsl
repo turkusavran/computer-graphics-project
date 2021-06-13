7b3e506e8dc0f7016efe367af8e09be0eeeabc5e
Selin Kaya <selinkaya99@gmail.com>
2021-06-14 01:31:24 +0300

    Changes on main.cpp

---
+++ FireSmokeProject_VisualStudio/FireSmokeProject/FireSmokeProject/vshader.glsl
@@ -0,0 +1,20 @@
+#version 410
+
+in vec4 vPosition;
+in vec3 vNormal;
+
+uniform mat4 ModelView;
+uniform mat4 Projection;
+
+// Texture Mapping
+in vec2 vTexCoord;
+out vec2 texCoord;
+
+void main() 
+{
+    // Transform vertex position into camera (eye) coordinates
+    vec3 pos = (ModelView * vPosition).xyz;
+
+    texCoord = vTexCoord;
+    gl_Position = Projection * ModelView * vPosition;
+}
